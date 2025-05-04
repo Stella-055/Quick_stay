b22b45682e5557468c920627b178b309f53d53a6
Stella-055 <stellaongayo243@gmail.com>
2025-05-04 23:30:02 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import { useLocation ,Routes,Route} from 'react-router-dom'
 import Footer from './Components/Footer'
 import AllRooms from './Pages/AllRooms'
 import Roomdetails from './Pages/Roomdetails'
+import MyBookings from './Pages/MyBookings'
+import HotelReg from './Components/HotelReg'
+import Layout from './Pages/HotelOwner/Layout'
+import Dashboard from './Pages/HotelOwner/Dashboard'
+import ListRoom from './Pages/HotelOwner/ListRoom'
+import AddRoom from './Pages/HotelOwner/AddRoom'
+
 
 
 function App() {
@@ -13,12 +20,18 @@ const IsOwner= useLocation().pathname.includes("owner")
   return (
   <div>
    {!IsOwner && < Navbar />} 
+   { false&&< HotelReg/>}
    <div className='min-h-[70vh]'>
     <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/rooms' element={<AllRooms/>} />
         <Route path='/rooms/:id' element={<Roomdetails/>} />
-        
+         <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/owner' element={<Layout/>}>
+           <Route index element={<Dashboard/>}/>
+           <Route path='add-room' element={<AddRoom/>}/>
+           <Route path="list-room"element={<ListRoom/>}/>
+        </Route>
       </Routes>
    </div>
    <Footer/>
---
+++ src/Pages/HotelOwner/Dashboard.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Title from '../../Components/Title'
+import { assets ,dashboardDummyData} from '../../assets/assets';
+
+const Dashboard = () => {
+    const[dashboarddata, setDashboarddata]=useState(dashboardDummyData)
+  return (
+    <div>
+      <Title align="left " font="outfit" title="Dashboard" subtitle="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil eos commodi corporis voluptatibus, vero sit similique? Nam voluptates officiis tenetur distinctio deserunt repudiandae, optio, hic voluptatibus asperiores atque deleniti laboriosam!" />
+       <div className='flex gap-4 my-8'>
+          <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+             <img src={assets.totalBookingIcon} alt=""  className='max-sm:hidden h-10'/>
+               <div className='flex flex-col sm:ml-4 font-medium'>
+                  <p className='text-blue-500 text-lg'>Total Bookings</p>
+                  <p className='text-neutral-400 text-base'>{dashboarddata.totalBookings}</p>
+                </div>
+         </div>
+         <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+            <img src={assets.totalRevenueIcon} alt=""  className='max-sm:hidden h-10'/>
+            <div className='flex flex-col sm:ml-4 font-medium'>
+                <p className='text-blue-500 text-lg'>Total Revenue</p>
+                <p className='text-neutral-400 text-base'>${dashboarddata.totalRevenue}</p>
+            </div>
+        </div>
+       </div>
+       <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Recent Bookings</h2>
+       <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+           <table className='w-full'>
+            <thead className='bg-gray-50'>
+                <tr>
+                    <th className='py-3 px-4 text-gray-800 font-medium'>User Name</th>
+                    <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Room Name</th>
+                    <th className='py-3 px-4 text-gray-800 font-medium text-center'>Total Amount</th>
+                    <th className='py-3 px-4 text-gray-800 font-medium text-center'>Payment Status</th>
+                </tr>
+            </thead>
+            <tbody className='text-sm'>
+                {dashboarddata.bookings.map((item,index)=>(
+                    <tr key={index}>
+                        <td className='py-3 px-4 text-gray-700border-gray-300'>{item.user.username}</td>
+                        <td className='py-3 px-4 text-gray-700border-gray-300 max-sm:hidden'>{item.room.roomType}</td>
+                    </tr>
+                ))}
+            </tbody>
+           </table>
+       </div>
+       
+    </div>
+  )
+}
+
+export default Dashboard
