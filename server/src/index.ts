22ba04d4642c2268310c6b9e2ecc786fef84e679
Stella-055 <stellaongayo243@gmail.com>
2025-09-02 22:46:07 +0300

    basicserver setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import  type { Request,Response } from "express"
 import express from "express"
+import cors from "cors"
 const app = express()
- 
+app .use (express.json())
+app .use (cors(
+    {origin:["http://localhost:3000","https://quickstay.vercel.app"],
+        credentials: true,
+    }
+
+  ))
 app.get("/", (req:Request,res:Response)=>{
     res.send("welcome to quick stay")
 })
