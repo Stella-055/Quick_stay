dc34b6ac34e4c9cfae6a6315aa029963c2076c25
Stella-055 <stellaongayo243@gmail.com>
2025-09-02 22:32:02 +0300

    backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import  type { Request,Response } from "express"
+import express from "express"
+const app = express()
+ 
+app.get("/", (req:Request,res:Response)=>{
+    res.send("welcome to quick stay")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`serer is upand running on port ${PORT}`)
+})
