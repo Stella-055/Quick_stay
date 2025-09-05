0f3c47d8c18ab9d97b48ab82be9bc09090edf113
Stella-055 <stellaongayo243@gmail.com>
2025-09-05 23:01:37 +0300

    added clerk

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import  type { Request,Response } from "express"
 import express from "express"
 import cors from "cors"
 import { dbConnect } from "./utils/db.js"
+import { clerkMiddleware } from '@clerk/express'
 dbConnect()
 const app = express()
+app.use(clerkMiddleware())
 app .use (express.json())
 app .use (cors(
     {origin:["http://localhost:3000","https://qshotel.netlify.app/"],
