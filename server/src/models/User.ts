c23f502f732bcf165c7fabc9a94bc779ebb30188
Stella-055 <stellaongayo243@gmail.com>
2025-09-05 22:38:49 +0300

    setup schema and model

---
+++ server/src/models/User.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema({
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: true },
+    role: { type: String, enum:["user","hotelowner"], default:"user" },
+    recentSerachedCities: [{ type: String, required: true }],
+},
+{timestamps:true});
+
+const User = mongoose.model("User", userShema);
+
+export default User;
