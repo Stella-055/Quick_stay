7152283668db0cbb0496b80a1da02c05ae0068fc
Stella-055 <stellaongayo243@gmail.com>
2025-11-02 11:00:22 +0300

    integrated owner dashboard endpoints

--- client/src/Components/Featuredest.jsx
+++ client/src/Components/Featuredest.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Hotelcard from "./Hotelcard";
-import { roomsDummyData } from "../assets/assets";
+
 import Title from "./Title";
 import { useNavigate } from "react-router-dom";
-
+import api from "../config/api";
+import { useQuery } from "@tanstack/react-query";
 const Featuredest = () => {
   const navigate = useNavigate();
+
+const { data, isLoading, isError ,error} = useQuery({
+    queryKey: ['featuredDestinations'],
+    queryFn: async () => {
+      const response = await api.get('/rooms');
+      return response.data;
+    },
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>{error.message}|| something went wrong</div>;
+  }
   return (
     <div className="flex flex-col items-center justify-center px-6 md:px-16 lg:px-24  bg-slate-50 pb-10 pt-20">
       <Title
@@ -13,7 +29,7 @@ const Featuredest = () => {
         subtitle="Explore the best hotels in your favorite destinations and discover exciting homes and properties around the world "
       />
       <div className="flex  flex-wrap items-center justify-center gap-6 mt-20 ">
-        {roomsDummyData.slice(0, 4).map((room, index) => (
+        {data.slice(0, 4).map((room, index) => (
           <Hotelcard room={room} key={room._id} index={index} />
         ))}
       </div>
